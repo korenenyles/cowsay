f63d8a0e35b64e4d3002da2ecd3e3f84674c6627
Koren Nyles <korenenyles@gmail.com>
2020-05-12 12:52:29 -0400

    added cowwords function using subprocess

---
+++ bigcow/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from bigcow.models import CowInput
+
+class AddTextInput(forms.Form):
+    cow_input = forms.CharField()
--- bigcow/views.py
+++ bigcow/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
 from bigcow.models import CowInput
+from bigcow.forms import AddTextInput
+import subprocess
 
-# Create your views here.
+  
+
 def index(request):
-    cow_data = CowInput.objects.all()
-    return render(request, 'index.html', {'cow_data': cow_data})
+    if request.method == "POST":
+        form = AddTextInput(request.POST)
+        if form.is_valid():
+            cow_data = form.cleaned_data
+            CowInput.objects.create(
+                cow_input = cow_data['cow_input'] 
+            )
+            cowput = cow_words(cow_data['cow_input'])
+            form = AddTextInput()
+                     
+            
+            return render(request, 'index.html', {"form":form,  'cowput':cowput})
+        form = AddTextInput()
+    return render(request, 'index.html', {'form':form})
+
+def cow_words(string):
+    cow_output = subprocess.Popen(['cowsay', string], stdout=subprocess.PIPE).communicate()[0]
+    return cow_output.decode("utf8")
+
