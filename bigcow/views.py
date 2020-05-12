8ba9038680b7383c8b95be0763d1c5b9ef55617f
Koren Nyles <korenenyles@gmail.com>
2020-05-12 15:40:25 -0400

    added a history page

--- bigcow/views.py
+++ bigcow/views.py
@@ -19,9 +19,27 @@ def index(request):
             
             return render(request, 'index.html', {"form":form,  'cowput':cowput})
         form = AddTextInput()
-    return render(request, 'index.html', {'form':form})
+    return render(request, 'index.html', {'form':AddTextInput()})
 
 def cow_words(string):
     cow_output = subprocess.Popen(['cowsay', string], stdout=subprocess.PIPE).communicate()[0]
     return cow_output.decode("utf8")
 
+def history(request):
+    
+    cow_data = CowInput.objects.all()
+
+    cow_history = []
+    for cow in cow_data:
+        cow_history.append(cow)
+        
+    cow_history = cow_history[-10:]
+    cow_history.reverse()
+    
+    
+
+        
+        
+    
+
+    return render(request, 'history.html', {"cow_history":cow_history })
